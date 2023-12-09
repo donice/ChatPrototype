0bb069ce9d01a9c214028c313e3a6ec01c363635
drdonice <doniceubaru1@gmail.com>
2023-12-09 10:23:16 +0100

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Chief JR. on 09/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
