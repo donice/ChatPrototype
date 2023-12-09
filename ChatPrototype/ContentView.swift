45a478d55693da02b9b768a3923fd670d3150462
drdonice <doniceubaru1@gmail.com>
2023-12-09 17:28:05 +0100

    Made a simple color change

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("knock! knock!")
+                .padding()
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
+            Text("Who's there?")
         }
         .padding()
     }
